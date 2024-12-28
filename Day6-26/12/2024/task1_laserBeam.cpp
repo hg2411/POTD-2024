a9b722e56be2fdbdc9b0c075b9c7fbdc4ddf2dfc
hg2411 <himanigoyal764@gmail.com>
2024-12-28 21:41:25 +0530

    Day 6 POTD

--- Day6-26/12/2024/task1_laserBeam.cpp
+++ Day6-26/12/2024/task1_laserBeam.cpp
@@ -1,4 +1,23 @@
 //Day:6 Date:26-12-2024
 //Task1:Number of Laser Beams in a Bank
-//
+//https://leetcode.com/problems/number-of-laser-beams-in-a-bank/submissions/1490774822/
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int n=bank.size(),prev=0,curr=0,sum=0;
+        for(int i=0;i<n;i++){
+            curr=count(bank[i].begin(),bank[i].end(),'1');
+            if(curr>0){
+                sum+=prev*curr;
+                prev=curr;
+            }
+        }
+        return sum;
+    }
+};
+//Time complexity is O(m*n)
+//Space complexity is O(1)
